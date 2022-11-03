b1cd1172ff23c3d22f166c7060f5969169d88c9a
Markus6 <113332266+Markus6@users.noreply.github.com>
2022-11-03 22:23:17 +0100

    Skapat Add Travel UI

--- TravelPal/AddTravelWindow.xaml.cs
+++ TravelPal/AddTravelWindow.xaml.cs
@@ -11,17 +11,24 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TravelPal.Managers;
+using TravelPal.Model;
 
-namespace TravelPal
+namespace TravelPal;
+
+/// <summary>
+/// Interaction logic for AddTravelWindow.xaml
+/// </summary>
+public partial class AddTravelWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for AddTravelWindow.xaml
-    /// </summary>
-    public partial class AddTravelWindow : Window
+    private TravelsWindow travelsWindow;
+    private UserManager userManager;
+    private TravelManager travelManager;
+    public AddTravelWindow(TravelsWindow travelsWindow, UserManager userManager)
     {
-        public AddTravelWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        this.travelsWindow = travelsWindow;
+        this.userManager = userManager;
+        travelManager = ((User)userManager.SignedInUser).TravelManager;
     }
 }
