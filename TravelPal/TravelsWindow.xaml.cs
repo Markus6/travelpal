4ad825f994acc1a22c50634db0036e08333454cb
Markus6 <113332266+Markus6@users.noreply.github.com>
2022-11-04 00:45:52 +0100

    Lagt till funktion att ta bort resa

--- TravelPal/TravelsWindow.xaml.cs
+++ TravelPal/TravelsWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using TravelPal.Managers;
+using TravelPal.Model;
 
 namespace TravelPal;
 
@@ -22,12 +23,22 @@ public partial class TravelsWindow : Window
 {
     private MainWindow mainWindow;
     private UserManager userManager;
+    private TravelManager travelManager;
     public TravelsWindow(MainWindow mainWindow, UserManager userManager)
     {
         InitializeComponent();
         lblUser.Content = userManager.SignedInUser.Username;
         this.mainWindow = mainWindow;
         this.userManager = userManager;
+        travelManager = ((User)userManager.SignedInUser).TravelManager;
+
+        foreach (Travel travel in travelManager.Travels)
+        {
+            ListViewItem item = new ListViewItem();
+            item.Tag = travel;
+            item.Content = travel.GetInfo();
+            lvTravels.Items.Add(item);
+        }
     }
 
     private void btnUser_Click(object sender, RoutedEventArgs e)
@@ -47,5 +58,34 @@ public partial class TravelsWindow : Window
     public void addListViewItem(ListViewItem listViewItem)
     {
         lvTravels.Items.Add(listViewItem);
+    }
+
+    private void btnSignOut_Click(object sender, RoutedEventArgs e)
+    {
+        mainWindow.Show();
+        Close();
+    }
+
+    private void btnRemove_Click(object sender, RoutedEventArgs e)
+    {
+        if (lvTravels.SelectedItem != null)
+        {
+            Travel travelToRemove = travelManager.Travels[lvTravels.SelectedIndex];
+            travelManager.RemoveTravel(travelToRemove);
+            ListViewItem selectedItem = lvTravels.SelectedItem as ListViewItem;
+            lvTravels.Items.Remove(selectedItem);
+        }
+        else
+        {
+            MessageBox.Show("You have to select a travel to remove first!");
+        }
+    }
+
+    private void btnDetails_Click(object sender, RoutedEventArgs e)
+    {
+        Hide();
+        Travel travel = travelManager.Travels[lvTravels.SelectedIndex];
+        TravelDetailsWindow travelDetailsWindow = new TravelDetailsWindow(travel);
+        travelDetailsWindow.Show();
     }
 }
